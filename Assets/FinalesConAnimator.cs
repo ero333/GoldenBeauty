fea4bd10332d001bae0c0796ea27b7930a9c227b
Lexxus <leortega574@gmail.com>
2025-10-20 13:52:43 -0300

    + animacion sin frames

--- Assets/FinalesConAnimator.cs
+++ Assets/FinalesConAnimator.cs
@@ -9,77 +9,39 @@ public class FinalesConAnimator : MonoBehaviour
 {
 
     private PlayerInput playerInput;
-    private InputAction forwardAction;
-    private InputAction backAction;
+    private InputAction UpAction;
+    private InputAction DownAction;
 
     public Animator animator;
     public bool isUp;
-    public bool isDown;
-    public bool goesUp;
-    public bool goesDown;
 
     // Start is called before the first frame update
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
-        forwardAction = playerInput.actions.FindAction("PlayerMap/SpaceBoy");
-        backAction = playerInput.actions.FindAction("PlayerMap/Back");
+        UpAction = playerInput.actions.FindAction("PlayerMap/Arriba");
+        DownAction = playerInput.actions.FindAction("PlayerMap/Abajo");
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (forwardAction.triggered) // 0 = clic izquierdo
+
+
+        if (UpAction.triggered) // 0 = clic izquierdo
         {
             Debug.Log("hola");
-            if (isUp && !isDown && !goesUp && !goesDown)
-            {
-                goesDown = true;
-                goesUp = false;
-                isDown = false;
-                isUp = false;
-            }
+                animator.SetBool("isUp",false);
+        }
 
-            if (!isUp && isDown && !goesUp && !goesDown)
-            {
-                goesDown = true;
-                goesUp = true;
-                isDown = false;
-                isUp = false;
-            }
-
-            /*if (isUp && !isDown && !goesUp && !goesDown)
-            {
-                goesDown = true;
-                goesUp = false;
-                isDown = false;
-            }
-
-            if (isUp && !isDown && !goesUp && !goesDown)
-            {
-                goesDown = true;
-                goesUp = false;
-                isDown = false;
-            }*/
-
+        if (DownAction.triggered) // 0 = clic izquierdo
+        {
+            Debug.Log("holas");
+                animator.SetBool("isUp", true);
         }
 
     }
 
-    public void GoesUP()
-    {
-        goesDown = false;
-        goesUp = false;
-        isDown = false;
-        isUp = true;
-    }
-
-    public void GoesDown()
-    {
-        goesDown = false;
-        goesUp = false;
-        isDown = true;
-        isUp = false;
-    }
 
 }
