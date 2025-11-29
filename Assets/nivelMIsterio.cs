e3e445435e633f2602da076eb94892e5b2fecf11
Lexxus <leortega574@gmail.com>
2025-11-29 16:01:13 -0300

    + ultimos cambios

---
+++ Assets/nivelMIsterio.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nivelMIsterio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void CargarZona()
+    {
+        SceneManager.LoadScene("misterio");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
